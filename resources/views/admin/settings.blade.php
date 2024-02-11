25bd2a2bf23b2d38540dbdc76111cc69d861e0a0
R4Nilluminatixs <noinuse@example.com>
2024-02-11 13:14:34 -0800

    chore: fix settings endsection.

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -9,27 +9,23 @@
         <div class="card-body">
             <form action="{{ route('battlemetrics.admin.settings') }}" method="POST">
                 @csrf
-
                 <div class="mb-3">
-                    <label class="form-label" for="tokenInput">{{ trans('battlemetrics::admin.settings.token') }}*</label>
-                    <input type="text" class="form-control @error('token') is-invalid @enderror" id="tokenInput" name="token" placeholder="-" value="{{ old('token', setting('battlemetrics.token')) }}" aria-describedby="tokenInfo" required>
+                    <label class="form-label" for="tokenInput">{{ trans('battlemetrics::admin.settings.token') }}
+                        *</label>
+                    <input type="text" class="form-control @error('token') is-invalid @enderror" id="tokenInput"
+                           name="token" placeholder="-" value="{{ old('token', setting('battlemetrics.token')) }}"
+                           aria-describedby="tokenInfo" required>
 
                     @error('token')
                     <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
                     @enderror
 
-                    <small id="tokenInfo" class="form-text">{{ trans('battlemetrics::admin.settings.token_info') }}</small>
+                    <small id="tokenInfo"
+                           class="form-text">{{ trans('battlemetrics::admin.settings.token_info') }}</small>
                 </div>
 
                 <button type="submit" class="btn btn-primary">
                     <i class="bi bi-save"></i> {{ trans('messages.actions.save') }}
-                </button>
-            </form>
-
-        </div>
-    </div>
-@endsection
-s.actions.save') }}
                 </button>
             </form>
 
